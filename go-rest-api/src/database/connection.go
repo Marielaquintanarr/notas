5360e86139f5cef6f89f9b521cc152a4e2df3369
Marielaquintanar <marielaqntrr@gmail.com>
2025-05-26 22:39:57 -0600

    db

--- go-rest-api/src/database/connection.go
+++ go-rest-api/src/database/connection.go
@@ -8,7 +8,8 @@ import (
 )
 
 func GetConnection() *sql.DB {
-	connStr := "postgres://postgres:mariela2004@localhost:5432/postgres?sslmode=disable"
+	connStr := "postgresql://postgres_db_za89_user:DBkjYA4wKUrRsYhTNQ7VCVaD4KJPPmYF@dpg-d0qk2iumcj7s73e3tidg-a/postgres_db_za89"
+	// connStr := "postgres://postgres:mariela2004@localhost:5432/postgres?sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal("Error abriendo conexión:", err)
